ef8f5032b02337b54fa7c5a8b9b9690b8909e85b
Taha Kızmaz <tahakizmaz@gmail.com>
2024-05-03 10:17:34 +0100

    sadasd

--- payapp/apps.py
+++ payapp/apps.py
@@ -6,6 +6,5 @@ class PayappConfig(AppConfig):
     name = 'payapp'
 
     def ready(self):
-        #start apache thrift server
         from subprocess import Popen
         Popen(['python3', './thrift/TimestampHandlerServer/timestamphandlerserver.py'])
